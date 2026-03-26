a5de4923327d650266e01f556777fcf9bfa64cbe
Manoah Kinnaert <manoah.k@icloud.com>
2026-03-26 09:58:48 +0100

    Added private readline helper method

--- src/main/java/com/manoahkinnaert/heap/HeapMain.java
+++ src/main/java/com/manoahkinnaert/heap/HeapMain.java
@@ -1,8 +1,23 @@
 package com.manoahkinnaert.heap;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HeapMain {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    private static String readLine(String message) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(message);
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String test = readLine("test");
+        System.out.println(test);
     }
 }
