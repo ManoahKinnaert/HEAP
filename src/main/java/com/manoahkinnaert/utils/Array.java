2ccc2907dd009a141836a7c0c7bc604a1ed1a671
Manoah Kinnaert <manoah.k@icloud.com>
2026-03-27 14:09:21 +0100

    Added counter to array

--- src/main/java/com/manoahkinnaert/utils/Array.java
+++ src/main/java/com/manoahkinnaert/utils/Array.java
@@ -3,18 +3,26 @@ package com.manoahkinnaert.utils;
 import java.util.ArrayList;
 
 public class Array extends ArrayList<Integer> {
+    Counter counter;
 
     public Array() {
         super();
+        counter = new Counter();
     }
 
     public void exch(int a, int b) {
         Integer obj = get(a);
         set(a, get(b));
         set(b, obj);
+        counter.increaseExchanges();
     }
 
     public boolean less(int a, int b) {
+        counter.increaseCompares();
         return get(a) < get(b);
+    }
+
+    public void resetCounter() {
+        counter.reset();
     }
 }
