b70da5832af2a7af6a24b85df386b836d3c1bd7d
Manoah Kinnaert <manoah.k@icloud.com>
2026-03-27 14:07:49 +0100

    Implemented heap swim and insert

--- src/main/java/com/manoahkinnaert/core/Heap.java
+++ src/main/java/com/manoahkinnaert/core/Heap.java
@@ -3,4 +3,22 @@ package com.manoahkinnaert.core;
 import com.manoahkinnaert.utils.Array;
 
 public class Heap extends Array {
+
+    public Heap() {
+        super();
+    }
+
+    // heap swim operation
+    private void swim(int k) {
+        while (k > 1 && less(k, k / 2)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    public void insert(int a) {
+        int n = size();
+        add(++n, a);
+        swim(n);
+    }
 }
