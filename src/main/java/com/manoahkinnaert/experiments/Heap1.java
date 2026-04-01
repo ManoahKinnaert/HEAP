94a3c7b4e169b8fa17eabcb260d69ae1dcdfd3ab
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-01 15:04:29 +0200

    some cleaning

--- src/main/java/com/manoahkinnaert/experiments/Heap1.java
+++ src/main/java/com/manoahkinnaert/experiments/Heap1.java
@@ -15,9 +15,8 @@ public class Heap1 extends HeapExp {
 
         // generate worst case input
         int[] array = new int[100];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
             array[i] = i;
-        }
 
         for (int j : array) {
             heap.insert(j);
@@ -26,19 +25,14 @@ public class Heap1 extends HeapExp {
         }
 
         int[] sizes = new int[100];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
             sizes[i] = i + 1;
-        }
-
-        for (int n = 1; n <= 100; n++) {
+        
+        for (int n = 1; n <= 100; n++)
             comparesTheoretical.add((int) (Math.log(n) / Math.log(2)));
-        }
-
-        XYSeriesCollection comparesSet =
-            generateDataset("Compares", sizes, compares);
-
-        XYSeriesCollection theoreticalSet =
-            generateDataset("Compares Theoretical", sizes, comparesTheoretical);
+        
+        XYSeriesCollection comparesSet = generateDataset("Compares", sizes, compares);
+        XYSeriesCollection theoreticalSet = generateDataset("Compares Theoretical", sizes, comparesTheoretical);
 
         new Chart("HEAP - 1", comparesSet, theoreticalSet);
     }
