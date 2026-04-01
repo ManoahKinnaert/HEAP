7c9ff60c069a804897cf1b5c7f26bfdacf5b2c37
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-01 15:09:22 +0200

    Cleanup Counter

--- src/main/java/com/manoahkinnaert/utils/Counter.java
+++ src/main/java/com/manoahkinnaert/utils/Counter.java
@@ -2,31 +2,20 @@ package com.manoahkinnaert.utils;
 
 public class Counter {
     private int compares;
-    private int exchanges;
 
     public Counter() {
         compares = 0;
-        exchanges = 0;
     }
 
     public int getCompares() {
         return compares;
     }
 
-    public int getExchanges() {
-        return exchanges;
-    }
-
     public void increaseCompares() {
         compares += 1;
     }
 
-    public void increaseExchanges() {
-        exchanges += 1;
-    }
-
     public void reset() {
         compares = 0;
-        exchanges = 0;
     }
 }
