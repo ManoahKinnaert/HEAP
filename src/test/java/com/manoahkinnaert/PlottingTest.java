2f083580f3a6b94b8c46a0c7acfb7049425193bd
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-13 19:22:35 +0100

    fixed PlottingTest

--- src/test/java/com/manoahkinnaert/PlottingTest.java
+++ src/test/java/com/manoahkinnaert/PlottingTest.java
@@ -18,7 +18,8 @@ public class PlottingTest {
         ArrayList<Integer> test1 = new ArrayList<Integer>();
         test1.add(10); test1.add(20); test1.add(30);
 
-        XYSeriesCollection set1 = exp.generateDataset("TEST", new int[] {1, 2, 3}, test1);
+        XYSeriesCollection set1 = new XYSeriesCollection();
+        set1.addSeries(exp.generateDataset("TEST", new int[] {1, 2, 3}, test1));
         new Chart("HEAP-1", set1, set1);
     }
 }
